26620a14f9175138537145d3e8481b5520ea646e
CalirDeminar <edward.firespinner@gmail.com>
2023-03-05 18:51:57 +0000

    tweaks

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ pub mod utils;
 pub mod erosion;
 pub mod erosion_culm;
 pub mod graphing;
+pub mod viewer;
 use midpoint::midpoint_terrain::*;
 use erosion::erosion_mod;
 use graphing::matrix_graphing::*;
 use utils::matrix_utils::*;
 use erosion_culm::erosion_culmulative;
+use viewer::terrain_viewer::*;
 
 const TERRAIN_FILENAME: &str = "./test-terrain.csv";
-const ERROSION_FILENAME: &str = "./test-erosion.csv";
-const ERROSION_DIFF_FILENAME: &str = "./test-erosion-diff.csv";
+const ERODED_FILENAME: &str = "./test-erosion-culm-diff.csv";
 
 fn gen_terrain() {
     let matrix = new();
@@ -23,29 +24,34 @@ fn erode_terrain() {
     let matrix = read_matrix(TERRAIN_FILENAME, LEN);
     let eroded = erosion_mod::erode(matrix);
     
-    write_matrix(&eroded, ERROSION_FILENAME);
-    render_matrix(&eroded, &ERROSION_FILENAME.replace(".csv", ".png"));
+    write_matrix(&eroded,  "./test-erosion.csv");
+    render_matrix(&eroded, & "./test-erosion.png");
 }
 
 fn erode_culm_terrain() {
     let matrix = read_matrix(TERRAIN_FILENAME, LEN);
     let eroded = erosion_culmulative::erode(matrix.clone());
     let diff = (eroded - matrix.view()) * 100.0;
-    write_matrix(&diff, "./test-erosion-culm-diff.csv");
+    write_matrix(&diff, ERODED_FILENAME);
     render_matrix(&diff, &"./test-erosion-culm-diff.png");
 }
 
 fn diff_terrain() {
     let terrain = read_matrix(TERRAIN_FILENAME, LEN);
-    let eroded = read_matrix(ERROSION_FILENAME, LEN);
+    let eroded = read_matrix(ERODED_FILENAME, LEN);
     let diff = (eroded - terrain.view()) * 100.0;
-    write_matrix(&diff, ERROSION_DIFF_FILENAME);
-    render_matrix(&diff, &ERROSION_DIFF_FILENAME.replace(".csv", ".png"));
+    write_matrix(&diff, "./test-erosion-diff.csv");
+    render_matrix(&diff, &"./test-erosion-diff.png");
+}
+fn view_terrain() {
+    let terrain = read_matrix(TERRAIN_FILENAME, LEN);
+    view(&terrain);
 }
 fn main() {
     // gen_terrain();
     // erode_terrain();
-    erode_culm_terrain();
+    // erode_culm_terrain();
     // diff_terrain();
+    view_terrain();
     println!("----");
 }
